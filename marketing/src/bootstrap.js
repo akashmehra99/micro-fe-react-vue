8da746e316fa4985b7976fab7148c1ba7cffae8e
Akash Mehra <akash.mehra99@gmail.com>
2022-02-15 14:28:01 +0530

    Updated memory and brower router

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
 import { App } from "./App";
 
-const mount = (element) => {
-  ReactDOM.render(<App />, element);
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, element);
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const rootElm = document.getElementById("_marketing-dev-root");
   if (rootElm) {
-    mount(rootElm);
+    mount(rootElm, { defaultHistory: createBrowserHistory()});
   }
 }
 
